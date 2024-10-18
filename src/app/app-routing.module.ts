08e1a0431c221d5f0d67ffd141ddea83bbf48ba7
JJ1087 <20200708@uthh.edu.mx>
2024-10-18 14:21:04 -0600

    QM-7 terminacion de la interfaz del carrito de compras

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
-import { NgModule } from '@angular/core';
+//PRIMERA OPCION DONDE SE REDIRIGUE AUTOMATICAMENTE AL LOGIN
+/*import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
@@ -27,7 +28,9 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {}*/
+
+//SEGUNDA OPCION DONDE SI JALAN LAS TABS
 
 /*import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
@@ -49,3 +52,32 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule {}*/
+
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',  // Redirigir a la página de login al iniciar la app
+    pathMatch: 'full'      // Coincidencia exacta de la ruta vacía
+  },
+  {
+    path: 'login', 
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: '',  // Si rellenas este path la ruta deja de funcionar, enigma de la vida. 
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
+
+
