0a477ed4057ef3ccd9998cdffa4399a60c31dcfc
JJ1087 <20200708@uthh.edu.mx>
2024-11-08 19:00:18 -0600

    QM-12 Generar funciones stripe

--- src/app/components/pasarela-pagos/pasarela-pagos.component.ts
+++ src/app/components/pasarela-pagos/pasarela-pagos.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { PasarelaService } from './pasarela.service';
 
+import { ApplePayEventsEnum, GooglePayEventsEnum, PaymentFlowEventsEnum, PaymentSheetEventsEnum, Stripe } from '@capacitor-community/stripe';
+import { first, lastValueFrom } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-pasarela-pagos',
   templateUrl: './pasarela-pagos.component.html',
@@ -8,7 +13,6 @@ import { PasarelaService } from './pasarela.service';
 })
 export class PasarelaPagosComponent  implements OnInit {
   @Input() totalCompra: number = 0;
-
   isVisible = false;
 
   direcciones: any[] = [];
@@ -16,7 +20,17 @@ export class PasarelaPagosComponent  implements OnInit {
   colonia: string = "";
   idDireccion: string = "";
 
-  constructor(private pasarelaService: PasarelaService) { }
+  data: any = {};
+
+  constructor(
+    private pasarelaService: PasarelaService,
+    private http: HttpClient
+  ) {
+    Stripe.initialize({
+      publishableKey: environment.stripe.publishableKey,
+    });
+    
+   }
 
 
 //Logia de apertura y cierre del modal--------------------------
@@ -33,6 +47,13 @@ export class PasarelaPagosComponent  implements OnInit {
 
   ngOnInit(){
     this.obtenerDirecciones();
+
+    this.data = {
+      name: 'Jeziel',
+      email: '20200776@uthh.edu.mx',
+      amount: 1000000,
+      currency: 'mxn',
+    };
   }
 
   //Obtencion de direcciones---------------------------------
@@ -67,5 +88,100 @@ export class PasarelaPagosComponent  implements OnInit {
 
     
   }
+
+
+  
+//Funcion de metodo de PAGO stripe-----------------------------------
+  httpPost(body) {
+    return this.http.post<any>(environment.api + 'payment-sheet', body).pipe(first());
+  }
+
+
+//----------------------------------
+  async googlePay() {
+    const isAvailable = Stripe.isGooglePayAvailable().catch(() => undefined);
+    if (isAvailable === undefined) {
+      return;
+    }
+  
+    Stripe.addListener(GooglePayEventsEnum.Completed, () => {
+      console.log('GooglePayEventsEnum.Completed');
+    });
+    
+    const data$ = this.httpPost(this.data);
+
+    const { paymentIntent } = await lastValueFrom(data$);
+
+    await Stripe.createGooglePay({
+      paymentIntentClientSecret: paymentIntent,
+
+      paymentSummaryItems: [{
+        label: 'Esau',
+        amount: 1099.00
+      }],
+      merchantIdentifier: 'Esau',
+      countryCode: 'MX',
+      currency: 'MXN',
+    });
+
+    const result = await Stripe.presentGooglePay();
+    if (result.paymentResult === GooglePayEventsEnum.Completed) {
+      this.splitAndJoin(paymentIntent);
+    }
+  }
+
+ splitAndJoin(paymentIntent) {
+   const result = paymentIntent.split('').slice(0, 2).join('');
+   console.log(result);
+   return result;
+ }
+ 
+  
+ async paymentSheet() {
+  if (!this.data.amount) {
+    console.error('Error: datos de pago incompletos');
+    return;
+  }
+
+  try {
+    Stripe.addListener(PaymentSheetEventsEnum.Completed, () => {
+      console.log('PaymentSheetEventsEnum.Completed');
+    });
+
+    const data$ = this.httpPost(this.data);
+    console.log(data$)
+    const { paymentIntent, ephemeralKey, customer } = await lastValueFrom(data$);
+
+    const response = await lastValueFrom(data$);
+    console.log('Backend response:', response);
+    console.log(paymentIntent, 'ephemeral', ephemeralKey, 'customer',customer)
+    if (!paymentIntent || !ephemeralKey || !customer) {
+      console.error('Faltan datos de pago:', { paymentIntent, ephemeralKey, customer });
+      return;
+    }
+    try {
+      await Stripe.createPaymentSheet({
+        paymentIntentClientSecret: paymentIntent,
+        customerId: customer,
+        customerEphemeralKeySecret: ephemeralKey,
+        merchantDisplayName: 'QuickDH',
+      });
+    } catch (error) {
+      console.error('Error en createPaymentSheet:', error);
+      return; // Termina la función si hay un error
+    }
+    
+    
+
+    
+    const result = await Stripe.presentPaymentSheet();
+    if (result && result.paymentResult === PaymentSheetEventsEnum.Completed) {
+      this.splitAndJoin(paymentIntent);
+     
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
 
 }
